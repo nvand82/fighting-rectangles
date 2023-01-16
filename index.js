3879bb233c5b54dd66e245c3b03dc99204e663b7
nvand82 <118779614+nvand82@users.noreply.github.com>
2023-01-16 18:59:07 -0500

    phisics 1st law

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height);
+const gravity = .1
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
@@ -18,6 +19,12 @@ class Sprite {
     c.fillStyle = color;
     this.draw();
     this.position.y += this.velocity.y;
+    if(this.position.y + this.height  + this.velocity.y >= canvas.height){
+    this.velocity.y = 0 
+    }
+    else{
+      this.velocity.y += gravity
+    }
   }
 }
 const player = new Sprite({
