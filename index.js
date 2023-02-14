d497f77bc6afc36ee3a1e622cf4655d33622fe23
nvand82 <118779614+nvand82@users.noreply.github.com>
2023-02-14 08:21:32 -0500

    Walls

--- index.js
+++ index.js
@@ -20,9 +20,16 @@ class Sprite {
     this.draw();
     this.position.y += this.velocity.y;
     this.position.x += this.velocity.x;
-    if(this.position.x + this.width  + this.velocity.x >= canvas.width){
-      this.velocity.x = 0 
-      }
+    // right border
+    if (this.position.x >= 1000) {
+      this.velocity.x = 0;
+      this.position.x = 1000
+    }
+    // left border
+    if (this.position.x <= 0) {
+      this.velocity.x = 0;
+      this.position.x = 0;
+    }
     if(this.position.y + this.height  + this.velocity.y >= canvas.height){
     this.velocity.y = 0 
     }
