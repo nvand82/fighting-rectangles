6b00b69412d20d315abd129fd3bc3e38f13b267a
nvand82 <118779614+nvand82@users.noreply.github.com>
2023-05-22 19:07:14 -0400

    starting attacks

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
+var playerDirect = 0
+var rivalDirect = 1
 c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = .2
 class Sprite {
@@ -12,18 +14,54 @@ class Sprite {
     this.position = position;
     this.velocity = velocity;
     this.height = 64;
-    this.snoBlob = {
-      position : this.position,
-      height : 10,
-      width : 40
-    }
   }
   draw() {
     //draw constant
     c.fillRect(this.position.x, this.position.y, 32, this.height);
-    c.fillRect(this.snoBlob.position.x, this.snoBlob.position.y, this.snoBlob.width, this.snoBlob.height)
-    c.fillStyle = ("white")
+    c.fillStyle = "white"
     c.fillRect(canvas.width/2, canvas.height/2, 64, 32);
+  }
+  update(color){
+    //what color are they
+    c.fillStyle = color;
+    this.draw();
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+    // right border
+    if (this.position.x >= 1000) {
+      this.velocity.x = 0;
+      this.position.x = 1000
+    }
+    // left border
+    if (this.position.x <= 0) {
+      this.velocity.x = 0;
+      this.position.x = 0;
+    }
+    //gravity this.position.x - this.width <= canvas.width/2
+    if(this.position.y + this.height  + this.velocity.y >= canvas.height){
+    this.velocity.y = 0 
+    }
+    else if(this.position.y + this.height < canvas.height/2 - 2 && this.position.y + this.height > canvas.height/2 - 32 && this.position.x >= canvas.width/2-32 &&
+      this.position.x <= canvas.width/2+64){
+        this.velocity.y = 0
+    }
+    else{
+      this.velocity.y += gravity
+      console.log(this.position.x)
+    }
+  }
+}
+class SnoBlob {
+  constructor({ position, velocity }) {
+    //carhicter size
+    this.position = position;
+    this.velocity = velocity;
+  }
+  draw() {
+    //draw constant
+    c.fillRect(this.position.x, this.position.y, 32, 16)
+    c.fillStyle = ("white")
+    
   }
   update(color){
     //what color are they
@@ -66,6 +104,11 @@ const rival = new Sprite({
   velocity: { x: 0, y: 10},
 });
 rival.draw();
+const PlayerSnoBlob = new SnoBlob({
+  position: { x: -30, y: 0},
+  velocity: { x:0, y:0}
+});
+ PlayerSnoBlob.draw();
 function animate(){
   window.requestAnimationFrame(animate);
   c.fillStyle = 'black';
@@ -86,30 +129,50 @@ window.addEventListener("keydown",(event) =>{
   switch(event.key){
 case'd':
 player.velocity.x=10
+playerDirect = 0
 break
 case'a':
 player.velocity.x=-10
+playerDirect = 1
 break 
 case'w':
 if (player.position.y >= canvas.height - player.height){//player.velocity.y < gravity
   // object not falling
-player.velocity.y=-10.7
+player.velocity.y=-10.68
 console.log(player.velocity.y)
 }
 break
+case's':
+if(playerDirect == 0){ 
+  PlayerSnoBlob.velocity.x =+ 15
+  PlayerSnoBlob.position.y = player.position.y
+  PlayerSnoBlob.position.x = player.position.x
+}
+else{
+  console.log(playerDirect)
+}
 case'ArrowRight':
 rival.velocity.x=10
+rivalDirect = 0
 break
 case'ArrowLeft':
 rival.velocity.x=-10
+rivalDirect = 1
 break 
 case'ArrowUp':
 if (rival.position.y >= canvas.height - rival.height){
   rival.velocity.y=-10.7
   }
 break
+case'ArrowDown':
+  if(rivalDirect == 0){
+    // snoblob right
+  }
+  else{
+    // left
+  }
+  break
 }
-
 })
 window.addEventListener("keyup",(event) =>{
   switch(event.key){
